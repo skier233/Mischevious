06a7c6425616225932639d15a12c57ccc0a4b7c1
Skier233 <benchy222@gmail.com>
2019-02-13 00:57:22 -0500

    Fixed mischevious bug

--- Structure/Modules/ModuleSelector.js
+++ Structure/Modules/ModuleSelector.js
@@ -1,41 +1,17 @@
-random = 0;
-var flag = true;
-while (flag)
-{
-    random = randomInteger(0, 4);
-    flag = false;
-    let folder = "";
-    if (random == 0)
-    {
-        folder = "Structure" + separator + "Modules" + separator + "ShortModules" + separator;
+DMessage("ModuleSelector start");
+    while (true) {
+        let folder = "Structure" + separator + "Modules" + separator +
+            ["ShortModules", "MediumModules", "LongModules", "Games", "Fetishes"][randomInteger(0, 4)]
+            + separator;
+        let files = listFilesInFolder(folder);
+        if (files == null) {
+            WMessage("File list is null while accessing'" + folder + "'likely a problem! Maybe directory missing/permissions?");
+        } else if (files.length == 0) {
+            // do nothing, we ignore an empty directory, it does not count.
+        } else {
+            DMessage("Running *.js from folder '" + folder + "'");
+            run(folder + "*.js");
+            break; // exit when one module was executed
+        }
     }
-    else if (random == 1)
-    {
-        folder = "Structure" + separator + "Modules" + separator + "MediumModules" + separator;
-    }
-    else if (random == 2)
-    {
-        folder = "Structure" + separator + "Modules" + separator + "LongModules" + separator;
-    }
-    else if (random == 3)
-    {
-        folder = "Structure" + separator + "Modules" + separator + "Games" + separator;
-    }
-    else if (random == 4)
-    {
-        folder = "Structure" + separator + "Modules" + separator + "Fetishes" + separator;
-    }
-    let files = listFilesInFolder(folder);
-    if (files == null)
-    {
-        DMessage("files is null. likely a problem!");
-    }
-    if (files.length == 0)
-    {
-        flag = true;
-    }
-    else
-    {
-        run(folder + "*.js");
-    }
-}
+DMessage("ModuleSelector end");
